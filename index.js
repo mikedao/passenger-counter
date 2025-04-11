195e41c13ede018bdd6539836b22324402041ea3
Michael Dao <mike@turing.edu>
2025-04-11 11:44:35 -0600

    Reassigning and Incrementing

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let myAge = 46;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
+// document.getElementById("count").innerText = 5
 
-console.log(myDogAge);
+let bonusPoints = 50;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
+
